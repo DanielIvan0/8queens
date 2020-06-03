653b7133596850717e3888056e50c57576e00e27
DanielIvan0 <danielivansaladotorres@gmail.com>
2020-06-03 14:10:31 -0500

    Efficiency errors fixed

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class Path:
         self._max = n
         self._path = []
         self._result = []
+        self._notAvSum = []
+        self._notAvRest = []
     @property
     def path(self):
         return self._path
@@ -14,18 +16,28 @@ class Path:
         return self._result
     def addPath(self, r):
         self._result.append(r.copy())
+    def append(self, node):
+        self._notAvSum.append(len(self._path) + node)
+        self._notAvRest.append(len(self._path) - node)
+        self._path.append(node)
+    def pop(self):
+        self._notAvSum.pop()
+        self._notAvRest.pop()
+        self._path.pop()
     def expandNode(self):
         response = []
         if len(self.path) < self._max:
             i = len(self._path)
+            #arr1 = [k + self._path[k] for k in range(len(self._path))]
+            #arr2 = [k - self._path[k] for k in range(len(self._path))]
             for j in range(self._max):
                 if not self.inPathOfDeath(i, j):
                     response.append(j)
         return response
     def inPathOfDeath(self, i, j):
         aux0 = j in self._path
-        aux1 = i + j in [k + self._path[k] for k in range(len(self._path))]
-        aux2 = i - j in [k - self._path[k] for k in range(len(self._path))]
+        aux1 = i + j in self._notAvSum
+        aux2 = i - j in self._notAvRest
         return aux0 or aux1 or aux2
     
 def queensDumbBattle(n):
@@ -35,13 +47,13 @@ def queensDumbBattle(n):
     print(f'result -> {path.result}')
 
 def expand(path, root):
-    path.path.append(root)
+    path.append(root)
     if len(path.path) == path.max:
         path.addPath(path.path)
     else:
         nudes = path.expandNode()
         for nude in nudes:
             expand(path, nude)
-    path.path.pop()
+    path.pop()
 
-queensDumbBattle(8)
+queensDumbBattle(5)
